a48edc05efafc5c772bbaeedd077ad21b25d90e9
Maxi <maxi.ottino@gmail.com>
2020-05-31 20:18:09 -0300

    verifico el rol y la operacion. Filter nuevo

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebPastas.Filters;
 
 namespace WebPastas.Controllers
 {
     public class HomeController : Controller
     {
+        [AutorizaUsuario(idOperacion:1)]
         public ActionResult Index()
         {
             return View();
         }
 
+        [AutorizaUsuario(idOperacion: 2)]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,6 +23,7 @@ namespace WebPastas.Controllers
             return View();
         }
 
+        [AutorizaUsuario(idOperacion: 5)]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
---
+++ Filters/AutorizaUsuario.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebPastas.Models;
+
+namespace WebPastas.Filters
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple =false)]
+    public class AutorizaUsuario:AuthorizeAttribute
+    {
+
+        private seg_usuario oUsuario;
+        private wi200122_pastas_tEntities db = new wi200122_pastas_tEntities();
+        private int idOperacion;
+
+        public AutorizaUsuario(int idOperacion = 0)
+        {
+            this.idOperacion = idOperacion;
+        }
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            String nombreOperacion = "";
+            String nombreModulo = "";
+            try
+            {
+                oUsuario = (seg_usuario)HttpContext.Current.Session["User"];
+                var lstMisOperaciones = from m in db.seg_rol_operacion
+                                        where m.rol_id == oUsuario.rol_id
+                                            && m.operacion_id == idOperacion
+                                        select m;
+
+
+                if (lstMisOperaciones.ToList().Count() == 0)
+                {
+                    var oOperacion = db.seg_operacion.Find(idOperacion);
+                    int? idModulo = oOperacion.modulo_id;
+                    nombreOperacion = getNombreDeOperacion(idOperacion);
+                    nombreModulo = getNombreDelModulo(idModulo);
+                    filterContext.Result = new RedirectResult("~/Error/SinAutorizacion?operacion=" + nombreOperacion + "&modulo=" + nombreModulo + "&msjeErrorExcepcion=");
+                }
+            }
+            catch (Exception ex)
+            {
+                filterContext.Result = new RedirectResult("~/Error/SinAutorizacion?operacion=" + nombreOperacion + "&modulo=" + nombreModulo + "&msjeErrorExcepcion=" + ex.Message);
+            }
+        }
+
+        public string getNombreDeOperacion(int idOperacion)
+        {
+            var ope = from op in db.seg_operacion
+                      where op.id == idOperacion
+                      select op.nombre;
+            String nombreOperacion;
+            try
+            {
+                nombreOperacion = ope.First();
+            }
+            catch (Exception)
+            {
+                nombreOperacion = "";
+            }
+            return nombreOperacion;
+        }
+
+        public string getNombreDelModulo(int? idModulo)
+        {
+            var modulo = from m in db.seg_modulo
+                         where m.id == idModulo
+                         select m.nombre;
+            String nombreModulo;
+            try
+            {
+                nombreModulo = modulo.First();
+            }
+            catch (Exception)
+            {
+                nombreModulo = "";
+            }
+            return nombreModulo;
+        }
+
+
+    }
+}
